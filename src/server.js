1353064d980a03f79e3a0a79fe61c0dca3301e39
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-06-16 13:48:19 +0200

    README.md completed

--- src/server.js
+++ src/server.js
@@ -26,10 +26,29 @@ app.use(session({
 
 const swaggerDocument = require('./swagger_output.json')
 app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.get('/api-docs', (req, res) => {
-  res.status(302).redirect('http://localhost:3000/swagger-ui')
-})
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid email or password
+ */
 app.post('/login', (req, res) => {
   const { email, password } = req.body
 
@@ -37,31 +56,64 @@ app.post('/login', (req, res) => {
 
   if (user) {
     req.session.user = email
-    res.send('Login successful')
+    res.status(200).send('Login successful')
   } else {
     res.status(401).send('Invalid email or password')
   }
 })
 
+/**
+ * @swagger
+ * /verify:
+ *   get:
+ *     summary: Verify user login
+ *     responses:
+ *       200:
+ *         description: User is logged in
+ *       401:
+ *         description: User not logged in
+ */
 app.get('/verify', (req, res) => {
   if (req.session.user != null) {
-    res.send(`User ${req.session.user} is logged in`)
+    res.status(200).send(`User ${req.session.user} is logged in`)
   } else {
     res.status(401).send('User not logged in')
   }
 })
 
+/**
+ * @swagger
+ * /logout:
+ *   delete:
+ *     summary: User logout
+ *     responses:
+ *       204:
+ *         description: Logged out successfully
+ *       500:
+ *         description: Error destroying session
+ */
 app.delete('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
       console.error('Error destroying session:', err)
       res.sendStatus(500)
     } else {
-      res.send('Logged out successfully')
+      res.status(204).send('Logged out successfully')
     }
   })
 })
 
+/**
+ * @swagger
+ * /tasks:
+ *   get:
+ *     summary: Get tasks
+ *     responses:
+ *       200:
+ *         description: List of tasks
+ *       403:
+ *         description: Forbidden, user not logged in
+ */
 app.get('/tasks', (req, res) => {
   if (req.session.user == null) {
     return res.sendStatus(403)
@@ -70,6 +122,25 @@ app.get('/tasks', (req, res) => {
   res.status(200).send(tasks)
 })
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   get:
+ *     summary: Get task by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Task found
+ *       403:
+ *         description: Forbidden, user not logged in or not authorized
+ *       404:
+ *         description: Task not found
+ */
 app.get('/tasks/:id', (req, res) => {
   if (req.session.user == null) {
     return res.sendStatus(403)
@@ -78,28 +149,55 @@ app.get('/tasks/:id', (req, res) => {
   const taskId = req.params.id
   const task = taskData.tasks.find((task) => task.id === taskId)
 
-  if (req.session.user !== task.User) {
-    return res.sendStatus(403)
-  }
-
   if (task) {
+    if (req.session.user !== task.User) {
+      return res.sendStatus(403)
+    }
     res.status(200).send(task)
   } else {
     res.status(404).send('Task was not found')
   }
 })
 
+/**
+ * @swagger
+ * /tasks:
+ *   post:
+ *     summary: Create a new task
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Titel:
+ *                 type: string
+ *               Beschreibung:
+ *                 type: string
+ *               Erstellungsdatum:
+ *                 type: string
+ *               Erfüllungsdatum:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Task created successfully
+ *       403:
+ *         description: Forbidden, user not logged in
+ */
 app.post('/tasks', (req, res) => {
   if (req.session.user == null) {
     return res.sendStatus(403)
   }
+
+  const { Titel, Beschreibung, Erstellungsdatum, Erfüllungsdatum } = req.body
   const newTask = {
     id: uuidv4(),
     User: req.session.user,
-    Titel: req.body.Titel ? req.body.Titel : null,
-    Beschreibung: req.body.Beschreibung ? req.body.Beschreibung : null,
-    Erstellungsdatum: req.body.Erstellungsdatum ? req.body.Erstellungsdatum : null,
-    Erfüllungsdatum: req.body.Erfüllungsdatum ? req.body.Erfüllungsdatum : null
+    Titel: Titel || null,
+    Beschreibung: Beschreibung || null,
+    Erstellungsdatum: Erstellungsdatum || null,
+    Erfüllungsdatum: Erfüllungsdatum || null
   }
 
   taskData.tasks.push(newTask)
@@ -107,6 +205,40 @@ app.post('/tasks', (req, res) => {
   res.status(201).json(newTask)
 })
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   put:
+ *     summary: Update task by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               Titel:
+ *                 type: string
+ *               Beschreibung:
+ *                 type: string
+ *               Erstellungsdatum:
+ *                 type: string
+ *               Erfüllungsdatum:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Task updated successfully
+ *       403:
+ *         description: Forbidden, user not logged in or not authorized
+ *       404:
+ *         description: Task not found
+ */
 app.put('/tasks/:id', (req, res) => {
   if (req.session.user == null) {
     return res.sendStatus(403)
@@ -130,6 +262,25 @@ app.put('/tasks/:id', (req, res) => {
   }
 })
 
+/**
+ * @swagger
+ * /tasks/{id}:
+ *   delete:
+ *     summary: Delete task by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Task deleted successfully
+ *       403:
+ *         description: Forbidden, user not logged in or not authorized
+ *       404:
+ *         description: Task not found
+ */
 app.delete('/tasks/:id', (req, res) => {
   if (req.session.user == null) {
     return res.sendStatus(403)
@@ -150,6 +301,11 @@ app.delete('/tasks/:id', (req, res) => {
   } else {
     res.status(404).send('Task was not found')
   }
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
 })
 
 app.listen(port, () => {
